cc1cf7a30278befc8e4963be58263f2bb62fb3b6
hijuliancode <julian.sosarojas@publicisgroupe.com>
2025-04-16 18:30:27 -0500

    Study: three.js: spaceship : POO

--- 00-spaceship-game/spaceship-code/src/main.ts
+++ 00-spaceship-game/spaceship-code/src/main.ts
@@ -3,32 +3,48 @@ import './style.css'
 import { Scene, PerspectiveCamera, WebGLRenderer, AmbientLight, DirectionalLight } from "three"
 import { GLTFLoader, GLTF } from 'three/examples/jsm/Addons.js';
 
-const canvas = document.getElementById("canvas") as HTMLCanvasElement;
-const scene = new Scene();
-const renderer = new WebGLRenderer({ canvas, antialias: true });
-const perspectiveCamera = new PerspectiveCamera(75, window.innerWidth / window.innerHeight, 0.1, 1000);
-const gltfLoader = new GLTFLoader();
+class App {
+  canvas = document.getElementById("canvas") as HTMLCanvasElement;
+  scene = new Scene();
+  renderer = new WebGLRenderer({ canvas: this.canvas, antialias: true });
+  perspectiveCamera = new PerspectiveCamera(75, window.innerWidth / window.innerHeight, 0.1, 1000);
+  gltfLoader = new GLTFLoader();
 
-renderer.setSize(window.innerWidth, window.innerHeight);
-perspectiveCamera.position.set(0, 8, -10);
-perspectiveCamera.lookAt(0, 0, 0);
+  constructor() {
+    this.config();
+    this.createLights();
+    this.loadModel();
+    this.animate();
+  }
 
-const ambientLight = new AmbientLight(0x404040, 2); // soft white light
-scene.add(ambientLight);
+  config() {
+    this.renderer.setSize(window.innerWidth, window.innerHeight);
+    this.perspectiveCamera.position.set(0, 8, -10);
+    this.perspectiveCamera.lookAt(0, 0, 0);
+  }
 
-const directionalLight = new DirectionalLight(0xffffff, 1);
-scene.add(directionalLight);
+  createLights() {
+    const ambientLight = new AmbientLight(0x404040, 2); // soft white light
+    this.scene.add(ambientLight);
 
-gltfLoader.load("/spaceship.glb", (gltf: GLTF) => {
-  const model = gltf.scene;
-  model.scale.set(0.5, 0.5, 0.5);
-  scene.add(model);
-})
+    const directionalLight = new DirectionalLight(0xffffff, 1);
+    this.scene.add(directionalLight);
+  }
 
-function animate() {
-  console.log("Animating...");
-  renderer.render(scene, perspectiveCamera)
-  requestAnimationFrame(animate);
+  loadModel() {
+    this.gltfLoader.load("/spaceship.glb", (gltf: GLTF) => {
+      const model = gltf.scene;
+      model.scale.set(0.5, 0.5, 0.5);
+      this.scene.add(model);
+    })
+  }
+
+  animate() {
+    console.log("Animating...");
+    this.renderer.render(this.scene, this.perspectiveCamera)
+    requestAnimationFrame(this.animate.bind(this));
+  }
+
 }
 
-animate();
+new App();
