e0db04804c31b4ab3c5f5d3ec8e7cb201525a87a
hijuliancode <julian.sosarojas@publicisgroupe.com>
2025-04-16 17:17:51 -0500

    Study: three.js: spaceship : initial config

--- 00-spaceship-game/spaceship-code/src/main.ts
+++ 00-spaceship-game/spaceship-code/src/main.ts
@@ -1,24 +1,18 @@
 import './style.css'
-import typescriptLogo from './typescript.svg'
-import viteLogo from '/vite.svg'
-import { setupCounter } from './counter.ts'
 
-document.querySelector<HTMLDivElement>('#app')!.innerHTML = `
-  <div>
-    <a href="https://vite.dev" target="_blank">
-      <img src="${viteLogo}" class="logo" alt="Vite logo" />
-    </a>
-    <a href="https://www.typescriptlang.org/" target="_blank">
-      <img src="${typescriptLogo}" class="logo vanilla" alt="TypeScript logo" />
-    </a>
-    <h1>Vite + TypeScript</h1>
-    <div class="card">
-      <button id="counter" type="button"></button>
-    </div>
-    <p class="read-the-docs">
-      Click on the Vite and TypeScript logos to learn more
-    </p>
-  </div>
-`
+import { Scene, PerspectiveCamera, WebGLRenderer } from "three"
 
-setupCounter(document.querySelector<HTMLButtonElement>('#counter')!)
+const canvas = document.getElementById("canvas") as HTMLCanvasElement;
+const scene = new Scene();
+const renderer = new WebGLRenderer({ canvas, antialias: true });
+
+const perspectiveCamera = new PerspectiveCamera(75, window.innerWidth / window.innerHeight, 0.1, 1000);
+renderer.setSize(window.innerWidth, window.innerHeight);
+
+function animate() {
+  console.log("Animating...");
+  renderer.render(scene, perspectiveCamera)
+  requestAnimationFrame(animate);
+}
+
+animate();
